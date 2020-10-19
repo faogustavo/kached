2081aea667ccf446046bcbe60a8da6e91ffeaae6
Gustavo Fao Valvassori <faogustavo@gmail.com>
2020-10-19 07:48:14 -0300

    converted kached into interface and extracted implementation

--- core/src/commonMain/kotlin/io/kached/Kached.kt
+++ core/src/commonMain/kotlin/io/kached/Kached.kt
@@ -1,120 +1,44 @@
 package io.kached
 
-import kotlin.reflect.KClass
-import kotlin.reflect.KType
+import io.kached.impl.KachedImpl
 import kotlin.reflect.typeOf
 
-@ExperimentalStdlibApi
-inline fun <reified V : Any> kached(block: Kached.Builder.() -> Unit): Kached<V> = Kached.Builder()
-    .apply(block)
-    .build()
+interface Kached<V : Any> {
+    suspend fun set(key: String, value: V)
+    suspend fun get(key: String): V?
+}
 
-@ExperimentalStdlibApi
-class Kached<V : Any> @PublishedApi internal constructor(
-    private val serializer: Serializer,
-    private val storage: Storage,
-    private val encryptor: Encryptor,
-    private val logger: Logger,
-    private val dataClass: KClass<V>,
-    private val dataType: KType,
-) {
+class KachedBuilder {
+    var serializer: Serializer = EmptySerializer
+    var storage: Storage = EmptyStorage
+    var encryptor: Encryptor = EmptyEncryptor
+    var logger: Logger = EmptyLogger
 
-    class Builder {
-        var serializer: Serializer = EmptySerializer
-        var storage: Storage = EmptyStorage
-        var encryptor: Encryptor = EmptyEncryptor
-        var logger: Logger = EmptyLogger
-
-        inline fun <reified V : Any> build(): Kached<V> = Kached<V>(
-            serializer = this.serializer,
-            storage = this.storage,
-            encryptor = this.encryptor,
-            logger = this.logger,
-            dataClass = V::class,
-            dataType = typeOf<V>()
-        )
-
-        fun copy(
-            serializer: Serializer = this.serializer,
-            storage: Storage = this.storage,
-            encryptor: Encryptor = this.encryptor,
-            logger: Logger = this.logger,
-        ) = Builder().apply {
-            this.serializer = serializer
-            this.storage = storage
-            this.encryptor = encryptor
-            this.logger = logger
-        }
-    }
-
-    suspend fun set(key: String, value: V) {
-        log("Kached -> set($key)")
-
-        val serializedValue = try {
-            serializer.serialize(value)
-        } catch (error: Throwable) {
-            log("Failed to serialize value with key = $key")
-            log(error)
-            return
-        }
-
-        val encryptedValue = try {
-            encryptor.encrypt(serializedValue)
-        } catch (error: Throwable) {
-            log("Failed to encrypt data with key = $key")
-            log(error)
-            return
-        }
-
-        try {
-            storage[key] = encryptedValue
-        } catch (error: Throwable) {
-            log("Failed to store data with key = $key")
-            log(error)
-        }
-    }
-
-    suspend fun get(key: String): V? {
-        log("Kached -> get($key)")
-
-        val valueFromStorage = try {
-            storage[key]
-        } catch (error: Throwable) {
-            log("Failed to acquire data from storage with key = $key")
-            log(error)
-            return null
-        }
-
-        if (valueFromStorage == null) {
-            log("There is no value for key = $key")
-            return null
-        }
-
-        val decryptedValue = try {
-            encryptor.decrypt(valueFromStorage)
-        } catch (error: Throwable) {
-            log("Failed to decrypt data with key = $key")
-            log(error)
-            return null
-        }
-
-        return try {
-            serializer.deserialize<V>(decryptedValue, dataClass, dataType)
-        } catch (error: Throwable) {
-            log("Failed to deserialize data with key = $key")
-            log(error)
-            null
-        }
-    }
-
-    private suspend fun log(message: String) = try {
-        logger.log(message)
-    } catch (error: Throwable) {
-        log(error)
-    }
-
-    private suspend fun log(error: Throwable) = try {
-        logger.log(error)
-    } catch (e: Throwable) {
+    fun copy(
+        serializer: Serializer = this.serializer,
+        storage: Storage = this.storage,
+        encryptor: Encryptor = this.encryptor,
+        logger: Logger = this.logger,
+    ) = KachedBuilder().apply {
+        this.serializer = serializer
+        this.storage = storage
+        this.encryptor = encryptor
+        this.logger = logger
     }
 }
+
+@ExperimentalStdlibApi
+inline fun <reified V : Any> kached(block: KachedBuilder.() -> Unit): Kached<V> = KachedBuilder()
+    .apply(block)
+    .buildKachedImpl()
+
+@PublishedApi
+@ExperimentalStdlibApi
+internal inline fun <reified V : Any> KachedBuilder.buildKachedImpl(): Kached<V> = KachedImpl<V>(
+    serializer = this.serializer,
+    storage = this.storage,
+    encryptor = this.encryptor,
+    logger = this.logger,
+    dataClass = V::class,
+    dataType = typeOf<V>()
+)
---
+++ core/src/commonMain/kotlin/io/kached/impl/KachedImpl.kt
@@ -0,0 +1,90 @@
+package io.kached.impl
+
+import io.kached.Encryptor
+import io.kached.Kached
+import io.kached.Logger
+import io.kached.Serializer
+import io.kached.Storage
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+open class KachedImpl<V : Any> @PublishedApi internal constructor(
+    private val serializer: Serializer,
+    private val storage: Storage,
+    private val encryptor: Encryptor,
+    private val logger: Logger,
+    private val dataClass: KClass<V>,
+    private val dataType: KType,
+) : Kached<V> {
+
+    override suspend fun set(key: String, value: V) {
+        log("Kached -> set($key)")
+
+        val serializedValue = try {
+            serializer.serialize(value)
+        } catch (error: Throwable) {
+            log("Failed to serialize value with key = $key")
+            log(error)
+            return
+        }
+
+        val encryptedValue = try {
+            encryptor.encrypt(serializedValue)
+        } catch (error: Throwable) {
+            log("Failed to encrypt data with key = $key")
+            log(error)
+            return
+        }
+
+        try {
+            storage[key] = encryptedValue
+        } catch (error: Throwable) {
+            log("Failed to store data with key = $key")
+            log(error)
+        }
+    }
+
+    override suspend fun get(key: String): V? {
+        log("Kached -> get($key)")
+
+        val valueFromStorage = try {
+            storage[key]
+        } catch (error: Throwable) {
+            log("Failed to acquire data from storage with key = $key")
+            log(error)
+            return null
+        }
+
+        if (valueFromStorage == null) {
+            log("There is no value for key = $key")
+            return null
+        }
+
+        val decryptedValue = try {
+            encryptor.decrypt(valueFromStorage)
+        } catch (error: Throwable) {
+            log("Failed to decrypt data with key = $key")
+            log(error)
+            return null
+        }
+
+        return try {
+            serializer.deserialize<V>(decryptedValue, dataClass, dataType)
+        } catch (error: Throwable) {
+            log("Failed to deserialize data with key = $key")
+            log(error)
+            null
+        }
+    }
+
+    private suspend fun log(message: String) = try {
+        logger.log(message)
+    } catch (error: Throwable) {
+        log(error)
+    }
+
+    private suspend fun log(error: Throwable) = try {
+        logger.log(error)
+    } catch (e: Throwable) {
+    }
+}
